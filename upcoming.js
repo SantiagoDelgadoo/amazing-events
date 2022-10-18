4a9ce5dac43885e57a0e3ea931e47fd2def763ac
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-10-17 21:27:21 -0300

    filtradoPorTexto

--- upcoming.js
+++ upcoming.js
@@ -1,12 +1,9 @@
-console.log(currentDate);
 let container = document.getElementById("cartas-home");
 for (i = 0; i < events.length; i++) {
-   
-    if (events[i].date > currentDate){
-    let cartas = document.createElement("div");
-    cartas.className = "card my-4";
-    cartas.innerHTML += `
-  <div class="card ">
+    if (events[i].date > currentDate) {
+        let cartas = document.createElement("div");
+        cartas.className = "card m-4";
+        cartas.innerHTML += `
                 <img src="${events[i].image}" class=" imgcards card-img-top" alt="${events[i].name}">
                 <div class="card-body text-center">
                     <h5 class="card-title text-center">${events[i].name}</h5>
@@ -16,20 +13,65 @@ for (i = 0; i < events.length; i++) {
                         <a href="details.html" class="boton btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
+
   `;
-  console.log(events[i].date);
-    container.appendChild(cartas);}
+
+        container.appendChild(cartas);
+    }
 }
 
-    let contenedorForm = document.getElementById("checkboxDeForm");
-    let arrayDeCategorias = events.map ((evento)=>evento.category)   //=> es = return
-    let arrayDeCategoriasSeteadas = new Set (arrayDeCategorias)
-    function imprimirCheckbox (categoria){
+
+let contenedorForm = document.getElementById("checkboxDeForm");
+let arrayDeCategorias = events.map((evento) => evento.category)   //=> es = return
+let arrayDeCategoriasSeteadas = new Set(arrayDeCategorias)
+function imprimirCheckbox(categoria) {
     contenedorForm.innerHTML += `
     <label class= "px-3" >${categoria}
     <input type="checkbox" name="${categoria}">
     </label>
     `;
 }
-arrayDeCategoriasSeteadas.forEach (imprimirCheckbox)
+arrayDeCategoriasSeteadas.forEach(imprimirCheckbox)
+
+
+
+
+
+
+
+
+let upcomingEvents = events.filter ((evento)=> evento.date > currentDate)
+
+let search = document.getElementById("enviarInfo")
+search.addEventListener("change", (evento) => {
+    let nombreFiltrado = evento.target.value
+    let nombreDeEventosFiltrados = upcomingEvents.filter((evento) => (evento.name.toLowerCase()).includes(nombreFiltrado.toLowerCase()))
+
+
+    container.innerHTML = " "
+
+
+
+
+    for (i = 0; i < nombreDeEventosFiltrados.length; i++) {
+        let cartas = document.createElement("div");
+        cartas.className = "card m-4";
+        cartas.innerHTML += `
+        <div class="card">
+            <img src="${nombreDeEventosFiltrados[i].image}" class=" imgcards card-img-top" alt="${nombreDeEventosFiltrados[i].name}">
+                    <div class="card-body text-center">
+                        <h5 class="card-title text-center">${nombreDeEventosFiltrados[i].name}</h5>
+                        <p class="card-text">${nombreDeEventosFiltrados[i].description}</p>
+                        <div class="d-flex justify-content-between ">
+                            <p>Price: ${nombreDeEventosFiltrados[i].price}</p>
+                        <a href="details.html" class="boton btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+        `;
+
+        container.appendChild(cartas);
+
+
+    }
+})
